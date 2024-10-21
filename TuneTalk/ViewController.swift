b4ff48d4a850945c80b95a2c652c89b04506432b
SteveMaglio <magliocchino.s@northeastern.edu>
2024-10-21 15:31:48 -0400

    first commit

---
+++ TuneTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TuneTalk
+//
+//  Created by Stephen Magliocchino on 10/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("STEVE TEST")
+    }
+
+
+}
+
